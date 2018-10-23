13f1e91c8a05de172475860a036989979472f410
scltc <github@scltc.club>
2018-10-22 22:09:46 -0700

    Start of multiple endpoint WebSocket support.

--- guest-web-server/webserver/src/main/java/com/brickintellect/webserver/WebSocketSession.java
+++ guest-web-server/webserver/src/main/java/com/brickintellect/webserver/WebSocketSession.java
@@ -1,6 +1,11 @@
 package com.brickintellect.webserver;
 
+import java.io.EOFException;
+import java.io.InputStream;
 import java.io.IOException;
+import java.io.UTFDataFormatException;
+import java.nio.CharBuffer;
+import java.nio.charset.StandardCharsets;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
@@ -15,6 +20,94 @@ import org.nanohttpd.protocols.websockets.WebSocket;
 import org.nanohttpd.protocols.websockets.WebSocketFrame;
 
 public class WebSocketSession extends WebSocket {
+
+    public static class WebSocketSessionInputStream extends ByteArrayInputStream {
+
+        char readOneCharacter(InputStream stream) throws IOException {
+
+            char result;
+
+            int theByte = stream.read();
+
+            if (theByte == -1) {
+                throw new EOFException("Endpoint: Improperly encoded.");
+            }
+
+            if ((theByte & 0x80) == 0) {
+                result = (char) theByte;
+            } else {
+                int remain;
+
+                if ((theByte & 0xE0) == 0xC0) {
+                    remain = 1;
+                    result = (char) (theByte & 0x1F);
+                } else if ((theByte & 0xF0) == 0xE0) {
+                    remain = 2;
+                    result = (char) (theByte & 0x0F);
+                } else if ((theByte & 0xF8) == 0xF0) {
+                    remain = 3;
+                    result = (char) (theByte & 0x07);
+                } else {
+                    throw new UTFDataFormatException("Endpoint: Improperly encoded.");
+                }
+
+                while (remain-- > 0) {
+                    if ((theByte = stream.read()) == -1) {
+                        throw new UTFDataFormatException("Endpoint: Improperly encoded.");
+                    }
+
+                    if ((theByte & 0xC0) != 0x80) {
+                        throw new UTFDataFormatException("Endpoint: Improperly encoded.");
+                    }
+
+                    result = (char) ((result << 6) | (theByte & 0x3F));
+                }
+            }
+
+            return result;
+        }
+
+        private final char endpoint;
+
+        public WebSocketSessionInputStream(byte[] buffer) throws IOException {
+
+            super(buffer);
+
+            endpoint = readOneCharacter(this);
+        }
+
+        public char getEndpoint() {
+            return endpoint;
+        }
+    }
+
+    public static class WebSocketSessionOutputStream extends ByteArrayOutputStream {
+
+        WebSocket socket;
+
+        WebSocketSessionOutputStream(WebSocket socket, char endpoint) throws IOException {
+            super(2048);
+            this.socket = socket;
+            write(StandardCharsets.UTF_8.encode(CharBuffer.wrap(new char[] { endpoint })).array());
+        }
+
+        @Override
+        public void flush() throws IOException {
+            if (socket == null) {
+                throw new IOException();
+            }
+            super.flush();
+            socket.send(toByteArray());
+        }
+
+        @Override
+        public void close() throws IOException {
+            if (socket != null) {
+                super.close();
+                socket = null;
+            }
+        }
+    }
 
     private static final Logger LOG = Logger.getLogger(WebSocketSession.class.getName());
 
