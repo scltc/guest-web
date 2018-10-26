d86cce29dc23c5b2ebce0583dc05b8446f74c3dd
scltc <github@scltc.club>
2018-10-25 22:41:04 -0700

    Work in progress.  WebSocket reconnect rewrite.

--- guest-web-site/src/app/core/json-rpc/MultiEndpointWebSocket.ts
+++ guest-web-site/src/app/core/json-rpc/MultiEndpointWebSocket.ts
@@ -5,72 +5,39 @@
 References:
   JSON-RPC 2.0 Specification
   https://www.jsonrpc.org/specification
+
   Auto-reconnecting and address-tracking WebSocket client
   https://jonaschapuis.com/2018/02/auto-reconnecting-and-address-tracking-websocket-client/
+
   Using WebSockets in Angular with RxJs WebSocketSubject
   https://medium.com/@alexdasoul/%D0%B8%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-websockets-%D0%B2-angular-c-rxjs-websocketsubject-5018ecc20ee5
   https://github.com/Angular-RU/angular-websocket-starter
 
-  WebSocketSubject does not reconnect with beta10 (change behaviour from beta7)
-  https://github.com/ReactiveX/rxjs/issues/1863
+  rxjs/webSocket
+  https://rxjs-dev.firebaseapp.com/api/webSocket/webSocket
 
-  https://www.learnrxjs.io/operators/error_handling/retrywhen.html
-  https://stackoverflow.com/questions/44911251/how-to-create-an-rxjs-retrywhen-with-delay-and-limit-on-tries
+  https://github.com/ReactiveX/rxjs/blob/master/src/internal/observable/dom/WebSocketSubject.ts
 */
 
-import { Observable, Observer, Subject, Subscriber, Subscription, interval, timer } from 'rxjs';
-import { distinctUntilChanged, filter, map, mergeMap, retryWhen, share, take, takeWhile } from 'rxjs/operators';
+import { Observable, Observer, Subject, Subscriber, Subscription, interval } from 'rxjs';
+import { distinctUntilChanged, filter, map, share, take, takeWhile } from 'rxjs/operators';
 import { WebSocketSubject, WebSocketSubjectConfig, webSocket } from 'rxjs/websocket';
 
-export interface JsonRpcError {
-    code: number;
-    message: string;
-    data?: any;
+export interface MultiEndpointWebSocketMessage {
+    endpoint: number;
+    payload: any;
 }
 
-export interface JsonRpcRequest {
-    jsonrpc: string;
-    method: string;
-    params?: any;
-    id?: number;
-}
-
-export interface JsonRpcResponse {
-    jsonrpc: string;
-    result?: any;
-    error?: JsonRpcError;
-    // 'id' isn't really optional, but must be marked that way to create
-    // the 'JsonRpcMessage' union below.
-    id?: number;
-}
-
-export type JsonRpcMessage
-    = JsonRpcError
-    | JsonRpcRequest
-    | JsonRpcResponse;
-
-export function isJsonRpcError(message: JsonRpcError | JsonRpcRequest | JsonRpcResponse): message is JsonRpcError {
-    return (message as JsonRpcError).code !== undefined;
-}
-
-export function isJsonRpcRequest(message: JsonRpcError | JsonRpcRequest | JsonRpcResponse): message is JsonRpcRequest {
-    return (message as JsonRpcRequest).method !== undefined;
-}
-
-export function isJsonRpcResponse(message: JsonRpcError | JsonRpcRequest | JsonRpcResponse): message is JsonRpcResponse {
-    return (message as JsonRpcError).code === undefined && (message as JsonRpcRequest).method === undefined;
-}
-
-export interface JsonRpcWebSocketConfiguration {
+export interface MultiEndpointWebSocketConfiguration {
     url: string;
     pingInterval?: number;
     reconnectionAttempts?: number;
     reconnectionInterval?: number;
 }
 
-export class JsonRpcWebSocket {
+export class MultiEndpointWebSocket {
 
-    private websocket$: WebSocketSubject<JsonRpcMessage>
+    private websocket$: WebSocketSubject<MultiEndpointWebSocketMessage>
         = null;
     private websocketconfiguration: WebSocketSubjectConfig<any>
     private reconnectionAttempts: number;
@@ -94,12 +61,10 @@ export class JsonRpcWebSocket {
         }
     }
 
-    private incoming$: Subject<JsonRpcMessage>
-        = new Subject<JsonRpcResponse>();
-    private messages$: Subscription
-        = new Subscription;
-    private outgoing$: Subject<JsonRpcRequest>
-        = new Subject<JsonRpcRequest>();
+    private incoming$: Subject<MultiEndpointWebSocketMessage>
+        = new Subject<MultiEndpointWebSocketMessage>();
+    private outgoing$: Subject<MultiEndpointWebSocketMessage>
+        = new Subject<MultiEndpointWebSocketMessage>();
 
     private connectedSubscriber: Subscriber<boolean>;
     private connectedSubscription: Subscription;
@@ -113,23 +78,11 @@ export class JsonRpcWebSocket {
     }).pipe(share(), distinctUntilChanged());
 
     private connect() {
-        this.websocket$ = webSocket<JsonRpcMessage>(this.websocketconfiguration)
-
-        this.messages$ = this.websocket$.pipe(
-            retryWhen(errors => errors.pipe(mergeMap(error => {
-                console.log('retryWhen: ' + JSON.stringify(error));
-                return timer(5 * 1000);
-            }))) /* ,
-            map(msg => {
-                console.log('msg: ' + JSON.stringify(msg));
-                return msg;
-            }) */
-        ).subscribe(message => {
-            console.log('message: ' + JSON.stringify(message));
-        });
+        this.websocket$ = webSocket<MultiEndpointWebSocketMessage>(this.websocketconfiguration);
 
         this.websocket$.subscribe(
             (message) => {
+/*
                 if (isJsonRpcRequest(message)) {
                     this.logMessage('request', message);
                 }
@@ -140,12 +93,13 @@ export class JsonRpcWebSocket {
                 else {
                     this.logMessage('error, ', message);
                 }
+*/
             },
             (error: Event) => {
                 this.logError('WebSocket error!', error);
                 /* if (!this.websocket$) */ {
                     // run reconnect if errors
-                    // this.reconnect();
+                    this.reconnect();
                 }
             },
             () => {
@@ -179,7 +133,7 @@ export class JsonRpcWebSocket {
         );
     }
 
-    constructor(configuration: JsonRpcWebSocketConfiguration) {
+    constructor(configuration: MultiEndpointWebSocketConfiguration) {
 
         // Create the WebSocketSubject configuration.
         this.websocketconfiguration = {
@@ -187,7 +141,7 @@ export class JsonRpcWebSocket {
             closeObserver: {
                 next: (event: CloseEvent) => {
                     this.logMessage('WebSocket.closeObserver()', event);
-                    // this.websocket$ = null;
+                    this.websocket$ = null;
                     this.connectedSubscriber.next(false);
                 }
             },
@@ -217,62 +171,10 @@ export class JsonRpcWebSocket {
             this.isConnected = isConnected;
 
             if (!this.reconnector$ && !isConnected) {
-                // this.reconnect();
+                this.reconnect();
             }
         });
 
         this.connect();
-    }
-
-    private id: number = 0;
-    /*
-        public call<TResponse, TRequest>(method: string, parameters: TRequest): Observable<TResponse> {
-            return Observable.create((observer: Observer<TResponse>) => {
-                let id = ++this.id;
-                const result = this.incoming$
-                    .pipe(
-                        filter(response => response.id === id && response.result),
-                        take(1),
-                        map(response => response.result))
-                    .pipe(
-                        filter(response => response.id === id && response.error),
-                        take(1),
-                        map(response => response.error)
-                    )
-                    .subscribe(observer);
-                this.websocket$.next({
-                    jsonrpc: '2.0',
-                    method: method,
-                    params: parameters,
-                    id: id
-                });
-                return result;
-            })
-        }
-    */
-    public call<TResponse>(method: string, parameters?: any): Observable<TResponse> {
-        return Observable.create((observer: Observer<TResponse>) => {
-            let id = ++this.id;
-            const result = this.incoming$
-                .pipe(
-                    filter(response => isJsonRpcResponse(response) && response.id === id),
-                    take(1),
-                    map(response => (response as JsonRpcResponse).result))
-                /*
-            .pipe(
-                filter(response => response.id === id && response.error),
-                take(1),
-                map(response => response.error)
-            )
-            */
-                .subscribe(observer);
-            this.websocket$.next({
-                jsonrpc: '2.0',
-                method: method,
-                params: parameters,
-                id: id
-            });
-            return result;
-        });
     }
 }
